67d0a240d584351adb409309e1e40a1985d78d1e
SairMontoya <131676347+SairMontoya@users.noreply.github.com>
2023-05-14 23:36:59 -0500

    Ejercicios funciones

--- Introduccion/intro1.py
+++ Introduccion/intro1.py
@@ -34,3 +34,7 @@ print(var)
 a = 3.0
 b = 4.0
 
+leg_a = float(input("Inserta la longitud del primer cateto: "))
+leg_b = float(input("Inserta la longitud del segundo cateto: "))
+hypo = (leg_a**2 + leg_b**2) ** .5
+print("La longitud de la hipotenusa es:", hypo)
